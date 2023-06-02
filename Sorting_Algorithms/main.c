89d6fdd85eab888c660adc19fab28ed18624de26
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-06-02 20:39:43 +0800

    Selection Sort

--- Sorting_Algorithms/main.c
+++ Sorting_Algorithms/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 void bubble_sort(int m[], int n);
+void selection_sort(int m[], int n);
 int main() {
-    printf("Enter n number:\n");
+    printf("Enter the value of 'n' to define the number of elements in this array:\n");
     int n;
     scanf("%d",&n);
     int m[n];
+    printf("Random numbers: ");
     for(int i=0; i<n;i++){
         m[i] = rand() %10;
         printf("%d ",m[i]);
     }
-    bubble_sort(m,n);
+    printf("\nChoose an option from the following list: \n \t 1 - Bubble Sort\n \t 2 - Selection Sort \n");
+    int option;
+    scanf("%d",&option);
+    switch (option) {
+        case 1:
+            bubble_sort(m,n);
+            break;
+        case 2:
+            selection_sort(m,n);
+            break;
+    }
     for (int i = 0; i < n; ++i) {
         printf(" \n %d ",m[i]);
     }
@@ -24,6 +36,21 @@ void bubble_sort(int m[], int n){
                 m[j] = m[j+1];
                 m[j+1] = temp;
             }
+        }
+    }
+}
+void selection_sort(int m[], int n){
+    for(int i=0;i<n-1;i++){
+        int min_p = i;
+        for (int j = i+1; j < n; ++j) {
+            if(m[j] < m[min_p]){
+                min_p = j;
+            }
+        }
+        if(min_p != i){
+            int temp = m[i];
+            m[i] = m[min_p];
+            m[min_p] = temp;
         }
     }
 }
